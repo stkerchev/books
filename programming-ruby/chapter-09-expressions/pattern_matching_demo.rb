9b65825af5ba6adba2a05dce88489b46e0799b67
stkerchev <stoyan.kerchev@gmail.com>
2023-04-17 20:22:49 +0300

    update pattern matching demo

--- programming-ruby/chapter-09-expressions/pattern_matching_demo.rb
+++ programming-ruby/chapter-09-expressions/pattern_matching_demo.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
-p 'Variable Binding'
+puts "\tVariable Binding"
 
 [1, "potato", 2, "potato"] => [first, String, second, String]
 puts "the numbers are #{first} and #{second}"
 
-{rank: "Ace", suit: "Hearts"} => {rank:, suit:}
+{ rank: "Ace", suit: "Hearts" } => { rank:, suit: }
 puts "Your card is the #{rank} of #{suit}."
+
+puts "\n\tCase Pattern Matching"
+input_str = {"employee": {"name":"Elmo","salary":56000,"married":true}}
+
+case input_str
+in { employee: { name:, salary:, married: } }
+  puts "#{name} earns #{salary} and is #{married ? "married" : "not married"}"
+else
+  puts "Unknown"
+end
+
+
