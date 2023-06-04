434a3e5149e59923f061008eeefa06a60fe4c7dd
stkerchev <stoyan.kerchev@gmail.com>
2023-06-04 20:26:57 +0300

    Added command expression demo

--- programming-ruby/chapter-09-expressions/command_expression_demo.rb
+++ programming-ruby/chapter-09-expressions/command_expression_demo.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
-puts "\tEnclose a string in backquotes (sometimes called backticks) or use the delimited from %x{...}"
+puts "\tCommand expression"
+puts "Enclose a string in backquotes (sometimes called backticks) or use the delimited from %x{...}"
 puts `date` # prints 4.06.2023 (нд) 13:19:04 EEST
 puts %x{date} # prints 4.06.2023 (нд) 13:19:04 EEST
+
+alias old_backquote `
+
+def `(command)
+  puts command
+  result = old_backquote(command)
+  unless Process.last_status.success?
+    puts "*** Command #{command} failed: status = #{Process.last_status.exitstatus}"
+  end
+  result
+end
+print `ls -l /etc/passwd`
+print `ls -l /etc/magic`
+# prints -rw-r--r-- 1 root root 2935 юни  2 23:24 /etc/passwd
